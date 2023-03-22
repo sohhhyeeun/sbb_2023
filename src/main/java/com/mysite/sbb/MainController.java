edee2b3a2466423ea2e37fc738ee511adcd7c8fa
sohhhyeeun <syeankm12076@gmail.com>
2023-03-22 11:41:48 +0900

    Regist Answer

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -12,6 +12,7 @@ public class MainController {
         return "안녕하세요 sbb에 오신 것을 환영합니다.";
     }
 
+    //root url
     @GetMapping("/")
     public String root() {
         return "redirect:/question/list";

d01dd5f7c2cc377d93ef36ac87cdf4d988bd4fe0
sohhhyeeun <syeankm12076@gmail.com>
2023-03-22 10:31:58 +0900

    Detail of question

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,13 +4,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
-@Controller
+@RequestMapping("/question")
 @RequiredArgsConstructor
+@Controller
 public class QuestionController {
+    /*
     private final QuestionRepository questionRepository;
 
     @GetMapping("/question/list")
@@ -20,5 +24,21 @@ public class QuestionController {
         //html에서 questionList를 사용하기 위해
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+    */
+    private final QuestionService questionService;
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionService.getList();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
+
+    @GetMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
